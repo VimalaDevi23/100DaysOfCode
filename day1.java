96a601f1dbd52bde4b17b71cdab85c1b474a0d9d
VimalaDevi23 <123957174+VimalaDevi23@users.noreply.github.com>
2023-12-10 00:35:17 +0530

    day1.java
    Started practice to improve the code skills from basic string concepts

--- day1.java
+++ day1.java
@@ -1,1 +1,24 @@
 
+https://leetcode.com/problems/merge-strings-alternately/?envType=study-plan-v2&envId=leetcode-75
+
+class Solution {
+    public String mergeAlternately(String word1, String word2) {
+
+    int maxLength = Math.max(word1.trim().length(),word2.trim().length());
+    
+    StringBuilder mergeString = new StringBuilder();
+    
+    for(int i=0;i<maxLength;i++){
+      if(i<word1.length()){
+        mergeString.append(word1.charAt(i));
+      }
+      if(i<word2.length()){
+        mergeString.append(word2.charAt(i));
+      }
+        
+    }
+    return (mergeString.toString());
+   
+        
+    }
+}

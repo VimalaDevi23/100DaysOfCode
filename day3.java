ea4419b18faad2bfe12e6a2e8a0e26a790691193
VimalaDevi23 <123957174+VimalaDevi23@users.noreply.github.com>
2023-12-12 21:51:03 +0530

    Update day3.java

--- day3.java
+++ day3.java
@@ -1,1 +1,12 @@
+https://leetcode.com/problems/kids-with-the-greatest-number-of-candies/description/?envType=study-plan-v2&envId=leetcode-75
+class Solution {
+    public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+        
+        int max = Arrays.stream(candies).max().getAsInt();
+        List<Boolean> listBoolValues = Arrays.stream(candies).
+        mapToObj(value -> value+extraCandies>=max).
+        collect(Collectors.toList());
 
+       return  listBoolValues;
+    }  
+}

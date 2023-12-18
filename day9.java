1be9a88386a66acedd3e9bbdfd923bf71ca61fc5
VimalaDevi23 <123957174+VimalaDevi23@users.noreply.github.com>
2023-12-18 08:19:55 -0800

    Create day9.java

---
+++ day9.java
@@ -0,0 +1,11 @@
+https://leetcode.com/problems/contains-duplicate/
+
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        
+   
+        return Arrays.stream(nums).distinct().count() != nums.length;
+    
+    }
+}
+

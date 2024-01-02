db9b57c8898fd124ee851b8d286f3593f4d55cdf
VimalaDevi23 <123957174+VimalaDevi23@users.noreply.github.com>
2024-01-02 09:07:01 -0800

    Create day24.java

---
+++ day24.java
@@ -0,0 +1,53 @@
+https://www.hackerrank.com/test/143hd7jsid6/questions/521e954e6ff11
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+
+class Result {
+
+    /*
+     * Complete the 'fizzBuzz' function below.
+     *
+     * The function accepts INTEGER n as parameter.
+     */
+
+    public static void fizzBuzz(int n) {
+    // Write your code here
+   IntStream.rangeClosed(1, n)
+                .mapToObj(i -> {
+                    if (i % 3 == 0 && i % 5 == 0) {
+                        return "FizzBuzz";
+                    } else if (i % 3 == 0) {
+                        return "Fizz";
+                    } else if (i % 5 == 0) {
+                        return "Buzz";
+                    } else {
+                        return Integer.toString(i);
+                    }
+                })
+                .forEach(System.out::println);
+
+    }
+
+}
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        Result.fizzBuzz(n);
+
+        bufferedReader.close();
+    }
+}
